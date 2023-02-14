353138cc22832695390726ad70ce263ce58dffd5
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-02-14 17:43:45 +0530

    Removed Voice Code

--- Package.swift
+++ Package.swift
@@ -18,11 +18,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "KitabooBookShelfFramework", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/KitabooBookShelfFramework.xcframework.zip", checksum: "73a5dcfb084e7b700d62e741711a04505258b6cd662341c05fe411fd57c58e3f"),
+        .binaryTarget(name: "KitabooBookShelfFramework", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/KitabooBookShelfFramework.xcframework.zip", checksum: "89fc2e06452ae05fcd01cb0208bc92563c209ec640f85c0013be660ebe057097"),
         
-        .binaryTarget(name: "Kitaboo_Reader_SDK", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/Kitaboo_Reader_SDK.xcframework.zip", checksum: "a1035f90106048ce7b9dd56738c3762846e2e22baf9334b14dc57d7f10582934"),
+        .binaryTarget(name: "Kitaboo_Reader_SDK", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/Kitaboo_Reader_SDK.xcframework.zip", checksum: "9b11e41e36c49b66005ed6194fdb508a3c1b8c49e3065ea2239b43834ac24b18"),
         
-        .binaryTarget(name: "KitabooSDKWithReader", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/KitabooSDKWithReader.xcframework.zip", checksum: "6a31c70258f0e481b347366371333e84fb5e95d27fe0133534ac5ef3ca04fb05"),
+        .binaryTarget(name: "KitabooSDKWithReader", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/KitabooSDKWithReader.xcframework.zip", checksum: "4a3c4c745bb61d674b02d0acf64d3bfd30f147469d94e4c38a8e0ce7813e8e70"),
         .target(
             name: "KitabooBookShelfRemote",
             dependencies: []),
