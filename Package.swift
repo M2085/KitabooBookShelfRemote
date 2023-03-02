b206fc698105a34d00a35f97c7afeb6ec9278b9e
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-03-03 00:11:48 +0530

    Added frameworks as embeded

--- Package.swift
+++ Package.swift
@@ -18,11 +18,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "KitabooBookShelfFramework", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/KitabooBookShelfFramework.xcframework.zip", checksum: "8cf1617507eef84b28dff5eec606e0623a04716d9ff3b1f1fe3923b6e5562bae"),
+        .binaryTarget(name: "KitabooBookShelfFramework", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/KitabooBookShelfFramework.xcframework.zip", checksum: "50d65506e882edf39016557f0974cfd4e0a127bf19da3824035e23cb366d1c94"),
         
-        .binaryTarget(name: "Kitaboo_Reader_SDK", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/Kitaboo_Reader_SDK.xcframework.zip", checksum: "50e49cac8073a14c4a5ce21bf9d3e6378cc9083694e9eacb133290aae4ea48d1"),
+        .binaryTarget(name: "Kitaboo_Reader_SDK", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/Kitaboo_Reader_SDK.xcframework.zip", checksum: "e6f463d4bcb809dd9ab822520ebfcf96841e5a4e7c3971259cbf9ac3cd5166d0"),
         
-        .binaryTarget(name: "KitabooSDKWithReader", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/KitabooSDKWithReader.xcframework.zip", checksum: "74bec3c2493d86296a803d16a7e28cafc212c67652194609ee028f83a5e583ff"),
+        .binaryTarget(name: "KitabooSDKWithReader", url: "https://qaread.kitaboo.com/reader/Sprint/githubfiles/GithubZip/KitabooSDKWithReader.xcframework.zip", checksum: "92fd73de15a226b8892160bc768ce7c6b30f1efa55e939f56bedbd7d064c48f4"),
         .target(
             name: "KitabooBookShelfRemote",
             dependencies: ["Kitaboo_Reader_SDK", "KitabooSDKWithReader"]),
